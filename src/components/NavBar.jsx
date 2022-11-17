151b45783af8c5987808e61f4ad15bf492d1fff7
Evgeny Polyakov <evg_pol@yahoo.com>
2022-11-17 22:32:19 +0300

    added banner section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,7 +34,7 @@ export const NavBar = () => {
                     <img src={logo} alt='Logo' />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" >
-                    <span className='navber-toggler-icon'></span>
+                    <span className='navbar-toggler-icon'></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
